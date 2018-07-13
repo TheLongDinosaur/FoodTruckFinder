b39d03d8fbf82943bd0fc414de0a6bf8664078a1
Max Liao <max.liao@gmail.com>
2018-07-12 20:41:46 -0400

    react routing

---
+++ client/src/components/SubmitBtn/SubmitBtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SubmitBtn.css";
+
+const submitBtn = props => (
+  <span className="submit-btn" {...props}>
+    Save
+  </span>
+);
+
+export default submitBtn;
